ca07e79cd06bf218157bcc606e076170dd8c1c80
Akash Kumar <93832364+aakashhkumar@users.noreply.github.com>
2023-11-20 09:38:59 +0530

    Update Design_themes.js

--- src/components/DesignThemes/Design_themes.js
+++ src/components/DesignThemes/Design_themes.js
@@ -6,7 +6,7 @@ function Design_themes(){
     const [arr,setArr] = useState([]);
 
     useEffect(()=>{
-        Axios.get("http://localhost:4000/weddingRoute/")
+        Axios.get("https://daydreamer-backend.onrender.com/weddingRoute/")
         .then((res)=>{
             if(res.status === 200)
                 setArr(res.data);
